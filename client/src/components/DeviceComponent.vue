5f68ee37e6a33af9a1cf8cc2a2e3fbe7751a2175
maforn <matteo.fornaini@studio.unibo.it>
2024-11-08 22:51:48 +0100

    Added find my device function

--- client/src/components/DeviceComponent.vue
+++ client/src/components/DeviceComponent.vue
@@ -2,50 +2,93 @@
   <div class="border shadow-md bg-white  rounded-lg p-4 w-full">
     <div class="flex items-center justify-between gap-2">
       <div class="flex items-center">
-        <v-icon :class="[{'-rotate-90': !expanded}, 'hover:cursor-pointer']" name="md-expandmore-round" scale="1.5" fill="gray" class="mr-2" @click.stop="expanded=!expanded"/>
+        <v-icon :class="[{'-rotate-90': !expanded}, 'hover:cursor-pointer']" name="md-expandmore-round" scale="1.5"
+                fill="gray" class="mr-2" @click.stop="expanded=!expanded"/>
         <v-icon v-if="isVulnerable" name="md-error" fill="red" scale="1.5"/>
         <v-icon v-else name="bi-check-circle-fill" fill="green" scale="1.5"/>
         <h2 class="text-2xl font-bold text-gray-900 mx-2">{{ device.name }}</h2>
       </div>
-      <div class="flex gap-2 items-center text-sm">
+      <div class="flex gap-2 items-center">
         <p class="text-gray-700 "><strong>IP:</strong> {{ device.ip }}</p>
-        <p class="text-gray-700"><strong>MAC:</strong> {{ device.mac }}</p>
+        <button @click="openModal" class="bg-blue-500 text-white px-4 py-2 rounded items-center flex">Find My Device
+          <v-icon name="md-search" scale="1.5" class="ml-2"/>
+        </button>
       </div>
     </div>
-    <ul class="flex gap-2 mt-3" v-if="expanded">
-      <li v-for="(service, index) in sortedServices" :key="index" :class="['px-4 py-2 rounded-lg mb-2', service.ifVulnerable ? 'bg-red-100' : 'bg-green-50']">
-        <p class="text-gray-800"><strong>Service:</strong> {{ service.name }}</p>
-        <p class="text-gray-800"><strong>Port:</strong> {{ service.port }}</p>
-        <p class="text-gray-800"><strong>Vulnerable:</strong> {{ service.ifVulnerable ? 'Yes' : 'No' }}</p>
-      </li>
-    </ul>
+    <div v-if="expanded">
+      <p class="text-gray-700 mt-3 ml-2"><strong>MAC:</strong> {{ device.mac }}</p>
+      <ul class="flex gap-2 mt-3">
+        <li v-for="(service, index) in sortedServices" :key="index"
+            :class="['px-4 py-2 rounded-lg mb-2', service.ifVulnerable ? 'bg-red-100' : 'bg-green-50']">
+          <p class="text-gray-800"><strong>Service:</strong> {{ service.name }}</p>
+          <p class="text-gray-800"><strong>Port:</strong> {{ service.port }}</p>
+          <p class="text-gray-800"><strong>Vulnerable:</strong> {{ service.ifVulnerable ? 'Yes' : 'No' }}</p>
+        </li>
+      </ul>
+    </div>
+    <ModalComponent v-if="showModal" @close="closeModal">
+      <template v-slot:header>
+        <h3 class="text-lg">Find The Device (IP: {{ device.ip }})</h3>
+      </template>
+      <template v-slot:body>
+        <Loader :messages="['Contacting the server...']" class="py-16" v-if="loading"/>
+        <div v-else>
+          <ol class="list-decimal list-inside space-y-2">
+            <li>Make a list of all the devices that could potentially be the one you're looking for.</li>
+            <li>Disconnect one device from your network that you think might be the culprit.</li>
+            <li>Use the "Scan" feature to check if the device is online.</li>
+            <li>If the device is still found, go back to step 2 and try disconnecting another device.</li>
+            <li>Continue this process until the scan feature says it's no longer online in the results.</li>
+          </ol>
+          <button @click="scanDevice" class="bg-blue-500 text-white px-4 py-2 rounded mt-4">Scan</button>
+          <p v-if="scanResult" class="mt-4">Scan Result: <span
+              :class="[scanResult === 'offline' ? 'text-emerald-500': 'text-red-500', 'font-bold', 'text-lg']">{{
+              scanResult
+            }}</span></p>
+        </div>
+      </template>
+      <template v-slot:footer>
+        <button @click="closeModal" class="bg-gray-500 text-white px-4 py-2 rounded">Close</button>
+      </template>
+    </ModalComponent>
   </div>
 </template>
 
 <script>
+import ModalComponent from "@/components/ModalComponent.vue";
+import axios from "axios";
+import Loader from "@/components/Loader.vue";
+
 export default {
+  components: {
+    Loader,
+    ModalComponent
+  },
   props: {
     device: {
       type: Object,
       required: true,
       validator: (value) => {
         return (
-          value.name &&
-          value.ip &&
-          value.mac &&
-          Array.isArray(value.services) &&
-          value.services.every(service =>
-            service.port &&
-            service.ifVulnerable !== undefined &&
-            service.name
-          )
+            value.name &&
+            value.ip &&
+            value.mac &&
+            Array.isArray(value.services) &&
+            value.services.every(service =>
+                service.port &&
+                service.ifVulnerable !== undefined &&
+                service.name
+            )
         );
       }
     }
   },
   data() {
     return {
-      expanded: false
+      expanded: false,
+      scanResult: null,
+      showModal: false,
+      loading: false,
     };
   },
   computed: {
@@ -54,6 +97,25 @@ export default {
     },
     sortedServices() {
       return this.device.services.sort((a, b) => b.ifVulnerable - a.ifVulnerable);
+    }
+  },
+  methods: {
+    openModal() {
+      this.showModal = true;
+    },
+    closeModal() {
+      this.showModal = false;
+      this.scanResult = null;
+    },
+    async scanDevice() {
+      try {
+        this.loading = true;
+        const response = await axios.get(`http://localhost:8000/isonline/${this.device.ip}`);
+        this.scanResult = response.data.status;
+      } catch (error) {
+        this.scanResult = 'error';
+      }
+      this.loading = false;
     }
   }
 };
