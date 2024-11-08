61cc464d116e73b75b7c1a41efbb32b8412ea0b6
matteo <matteo@HP>
2024-11-08 18:20:29 +0100

    Start scan api

--- server/main.py
+++ server/main.py
@@ -3,13 +3,10 @@ import uvicorn
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/start")
+def start_scan():
+    # start the scan
+    return {"message": "Scan started"}
 
 
 if __name__ == "__main__":
