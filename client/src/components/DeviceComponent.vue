3b4e87e746d9106470b4da99a4c164ca0145a6da
maforn <matteo.fornaini@studio.unibo.it>
2024-11-08 20:09:31 +0100

    Restyled devices again
    Author:    maforn <matteo.fornaini@studio.unibo.it>

--- client/src/components/DeviceComponent.vue
+++ client/src/components/DeviceComponent.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { defineProps } from 'vue';
+import {defineProps} from 'vue';
 
 const props = defineProps({
   device: {
@@ -7,15 +7,15 @@ const props = defineProps({
     required: true,
     validator: (value) => {
       return (
-        value.name &&
-        value.ip &&
-        value.mac &&
-        Array.isArray(value.services) &&
-        value.services.every(service =>
-          service.port &&
-          service.ifVulnerable !== undefined &&
-          service.name
-        )
+          value.name &&
+          value.ip &&
+          value.mac &&
+          Array.isArray(value.services) &&
+          value.services.every(service =>
+              service.port &&
+              service.ifVulnerable !== undefined &&
+              service.name
+          )
       );
     }
   }
@@ -23,15 +23,24 @@ const props = defineProps({
 </script>
 
 <template>
-  <div class="border bg-white dark:bg-gray-800 shadow-md rounded-lg p-4 m-2 w-11/12">
-    <h2 class="text-2xl font-bold mb-4 text-gray-900 dark:text-gray-100">{{ device.name }}</h2>
-    <p class="text-gray-700 dark:text-gray-300"><strong>IP:</strong> {{ device.ip }}</p>
-    <p class="text-gray-700 dark:text-gray-300"><strong>MAC:</strong> {{ device.mac }}</p>
-    <ul class="mt-4">
-      <li v-for="(service, index) in device.services" :key="index" class="bg-gray-100 dark:bg-gray-700 p-2 rounded mb-2">
+  <div class="border shadow-md bg-white dark:bg-gray-800 rounded-lg p-4 m-2 w-11/12">
+    <div class="flex items-center space-x-4 p-4 dark:bg-gray-700 rounded-lg">
+      <h2 class="text-2xl font-bold text-gray-900 dark:text-gray-100">{{ device.name }}</h2>
+      <p class="text-gray-700 dark:text-gray-300"><strong>IP:</strong> {{
+          device.ip
+        }}</p>
+      <p class="text-gray-700 dark:text-gray-300"><strong>MAC:</strong> {{
+          device.mac
+        }}</p>
+    </div>
+    <ul>
+      <li v-for="(service, index) in device.services" :key="index"
+          class="bg-gray-100 dark:bg-gray-700 p-2 rounded mb-2">
         <p class="text-gray-800 dark:text-gray-200"><strong>Service Name:</strong> {{ service.name }}</p>
         <p class="text-gray-800 dark:text-gray-200"><strong>Port:</strong> {{ service.port }}</p>
-        <p class="text-gray-800 dark:text-gray-200"><strong>Vulnerable:</strong> {{ service.ifVulnerable ? 'Yes' : 'No' }}</p>
+        <p class="text-gray-800 dark:text-gray-200"><strong>Vulnerable:</strong> {{
+            service.ifVulnerable ? 'Yes' : 'No'
+          }}</p>
       </li>
     </ul>
   </div>
